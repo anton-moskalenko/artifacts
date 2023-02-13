36b363495c86f6ca0d1a769fce39187a2b6d7e26
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-02-13 08:22:55 +0200

    [0.0.22] Add Interstate.Ticket.Save API method.

---
+++ Source/API/Ticket/Save/Method.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Liloi\Interstate\API\Ticket\Edit;
+
+use Liloi\API\Response;
+use Liloi\Interstate\API\Method as SuperMethod;
+use Liloi\Interstate\Engine\Domain\Ticket\Manager;
+use Liloi\Interstate\Engine\Domain\Ticket\Statuses;
+
+class Method extends SuperMethod
+{
+    public static function execute(): Response
+    {
+        $entity = Manager::load(self::getParameter('key'));
+
+        $entity->setTitle(self::getParameter('title'));
+        $entity->setStart(self::getParameter('start'));
+        $entity->setFinish(self::getParameter('finish'));
+        $entity->setStatus(self::getParameter('status'));
+        $entity->setType(self::getParameter('type'));
+        $entity->setData(self::getParameter('data'));
+
+        $entity->save();
+
+        return new Response();
+    }
+}
