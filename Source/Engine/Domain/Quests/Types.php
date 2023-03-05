3ba696a1741729b0f755e46429ea9b72afbf6598
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-03-05 02:50:08 +0200

    [0.4.20] Add several types.

--- Source/Engine/Domain/Quests/Types.php
+++ Source/Engine/Domain/Quests/Types.php
@@ -6,9 +6,11 @@ class Types
 {
     public const PROTOCOLS = 1;
     public const TRAINING = 2;
+    public const PROJECTS = 3;
 
     public static $list = [
         self::PROTOCOLS => 'Protocols',
         self::TRAINING => 'Training',
+        self::PROJECTS => 'Projects',
     ];
 }
