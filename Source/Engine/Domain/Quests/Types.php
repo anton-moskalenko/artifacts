53ab83bfd69b4065058a2372b45f3e1073a18eb2
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-03-08 21:30:25 +0200

    [0.5.19] Change quest types. {2023:10:3:11}

--- Source/Engine/Domain/Quests/Types.php
+++ Source/Engine/Domain/Quests/Types.php
@@ -7,18 +7,18 @@ class Types
     public const PROTOCOLS = 1;
     public const TRAINING = 2;
     public const PROJECTS = 3;
-    public const HOBBIES = 4;
+    public const SURVIVAL = 4;
     public const FAMILY = 5;
     public const EXAMS = 6;
-    public const MYTHS = 7;
+    public const WEAKNESSES = 7;
 
     public static $list = [
         self::PROTOCOLS => 'Protocols',
         self::TRAINING => 'Training',
         self::PROJECTS => 'Projects',
-        self::HOBBIES => 'Hobbies',
-        self::FAMILY => 'Family',
+        self::SURVIVAL => 'Survival',
         self::EXAMS => 'Exams',
-        self::MYTHS => 'Myths'
+        self::WEAKNESSES => 'Weaknesses',
+        self::FAMILY => 'Family'
     ];
 }
